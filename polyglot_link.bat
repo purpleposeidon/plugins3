bfbc92e4c96e5d8773c8231e6ca6043762a79db5
neptunepink <purpleposeidon@gmail.com>
2021-09-07 17:34:49 -0700

    The stub .bat's just break on windows.

--- polyglot_link.bat
+++ polyglot_link.bat
@@ -6,4 +6,3 @@ link.exe %*
 
 @REM Yes, this is cursed. .cargo/config lets you specify per-target, but not per-host.
 @REM Let us be gladdened that there's basically nothing to do in here.
-@REM The empty batch scripts `#!.bat` and `FILTER_LINUX.bat` should keep this batch script from generating errors.
