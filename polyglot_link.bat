b94ec3ed138f44500c4b59f000ff92d49825b663
neptunepink <purpleposeidon@gmail.com>
2021-09-07 18:53:02 -0700

    Fix polyglot_link.

--- polyglot_link.bat
+++ polyglot_link.bat
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-FILTER_LINUX="" exec ./clang/lld-link /LIBPATH:./msvc_x64 /LIBPATH:./msvc_vc_lib/ /LIBPATH:./msvc_ucrt
+FILTER_LINUX="" exec ./toolchain/lld-link /LIBPATH:./msvc_x64 /LIBPATH:./msvc_vc_lib/ /LIBPATH:./msvc_ucrt
 
 link.exe %*
 
