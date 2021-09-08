1b555aaf2286486bf9c005eba2c3055ff4475c1b
neptunepink <purpleposeidon@gmail.com>
2021-09-08 07:46:12 -0700

    s/msvc/wdk/

--- polyglot_link.bat
+++ polyglot_link.bat
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-FILTER_LINUX="" exec ./toolchain/lld-link /LIBPATH:"./msvc/Program Files/Windows Kits/10/Lib/10.0.15063.0/um/x64/" /LIBPATH:"./msvc/Program Files/Microsoft Visual Studio 14.0/VC/lib/amd64/" /LIBPATH:"./msvc/Program Files/Windows Kits/10/Lib/10.0.15063.0/ucrt/x64/" $@
+FILTER_LINUX="" exec ./toolchain/lld-link /LIBPATH:"./wdk/Program Files/Windows Kits/10/Lib/10.0.15063.0/um/x64/" /LIBPATH:"./wdk/Program Files/Microsoft Visual Studio 14.0/VC/lib/amd64/" /LIBPATH:"./wdk/Program Files/Windows Kits/10/Lib/10.0.15063.0/ucrt/x64/" $@
 
 link.exe %*
 
